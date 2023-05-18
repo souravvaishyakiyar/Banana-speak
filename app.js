43735fc04ce75a7a94153157ac40f8c54f8b1491
souravvaishyakiyar <souravvaishyakiyar@gmail.com>
2023-05-19 01:18:23 +0530

    Project done

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var btntranslate=document.querySelector("#btn-translate");
 var textinput=document.querySelector("#txt-input");
 //console.log(textinput);
+var output=document.querySelector("#output");
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+	
+function gettranslationURL(text){
+   return serverURL + "?" + "text=" + text
+}
+
+function errorhandler(error){
+   console.log("error occured",error)
+   alert("something wrong with server")
+}
 
 function clickhandler(){
 
-   console.log("clicked!");
-   console.log("input",textinput.value);
+ var inputtext = textinput.value;
+
+ fetch(gettranslationURL(inputtext))
+   .then(response=>response.json())
+   .then(json=>{
+      var translatedtext=json.contents.translated;
+      output.innerText=translatedtext;
+   })
+   .catch(errorhandler)
+   
 };
 
 
